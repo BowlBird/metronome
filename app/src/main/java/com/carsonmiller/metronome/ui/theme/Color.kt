54850cbae931f9492a7fea645c02205aa93aec20
Carson Miller <millercarson111@gmail.com>
2022-09-13 20:37:52 -0500

    -Updated Theme, Colors, and Typography for Jetpack Compose

--- app/src/main/java/com/carsonmiller/metronome/ui/theme/Color.kt
+++ app/src/main/java/com/carsonmiller/metronome/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.carsonmiller.metronome.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Gray800 = Color(0xFF424242)
+val Gray900 = Color(0xFF212121)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Brown300 = Color(0xFFA1887F)
+
+val BlueGray300 = Color(0xFF90A4AE)
--- app/src/main/java/com/carsonmiller/metronome/ui/theme/Type.kt
+++ app/src/main/java/com/carsonmiller/metronome/ui/theme/Type.kt
@@ -2,33 +2,38 @@ package com.carsonmiller.metronome.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.carsonmiller.metronome.R
+
+val arimo = FontFamily(
+    Font(R.font.arimo)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = arimo,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = arimo,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = arimo,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
-    )
-    */
+    ),
 )
